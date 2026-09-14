a7c16d5ed14cde4bb0f4f33aecb74de9d01d429b
Piyas Ahmed <apiyas761@gmail.com>
2026-09-14 08:14:16 +0600

    spelling

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -7,7 +7,7 @@ import SelectedStack from "./SelectedStack";
 import { toast } from "react-toastify";
 
 const technologiesFetch = async (): Promise<ITechnologiesType[]> => {
-  const res = await fetch("/Techmologies.json");
+  const res = await fetch("/Technologies.json");
   const data = await res.json();
   return data;
 };
